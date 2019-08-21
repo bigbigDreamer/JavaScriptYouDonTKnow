9b2b7c6acfc567466be5ec1974f7cc4b5476b58d
bigbigDreamer <1826001146@qq.com>
2019-08-21 09:17:51 +0800

    add content in this

---
+++ RollUp/prototype.js
@@ -0,0 +1,7 @@
+/**
+ * @time  2019/8/20 16:19
+ * @author  Eric Wang <vuejs@vip.qq.com>
+ * @desc    prototype
+ *          - 每一个函数都有自己的prototype
+ *          - 每一个对象都有自己的一个__proto__指向它的上级原型对象
+ */
