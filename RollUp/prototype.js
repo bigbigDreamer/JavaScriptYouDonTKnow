7c80a49224f661073bd00aa5db9fc87b6910d13e
bigbigDreamer <1826001146@qq.com>
2019-08-22 09:37:42 +0800

    新增了原型链

--- RollUp/prototype.js
+++ RollUp/prototype.js
@@ -4,4 +4,23 @@
  * @desc    prototype
  *          - 每一个函数都有自己的prototype
  *          - 每一个对象都有自己的一个__proto__指向它的上级原型对象
+ *
+ *          ----------------------------------------------------
+ *          再者理解：JS里面，万物皆对象，但是函数是创世之神
+ *          Because   : Object.constructor === Function;
+ *          也可以这样理解，所有构造器的constructor都指向Function
  */
+
+function Foo() {
+
+}
+
+const f = new Foo();
+
+console.log(f.__proto__ === Foo.prototype);
+
+console.log(Foo.prototype.__proto__ === Object.prototype);
+
+console.log(Object.prototype.__proto__ === null);
+
+console.log(Function.prototype.__proto__ === Object.prototype)
